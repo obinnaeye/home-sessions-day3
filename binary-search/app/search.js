5d4e32558c4cd8b195c200a3112a97009f2bc10c
Nnenanya Obinna Kingsley <obinnaeye@yahoo.com>
2017-03-15 06:58:59 +0000

    add prototype toTwenty, toForty, and toOneThousand

--- binary-search/app/search.js
+++ binary-search/app/search.js
@@ -3,3 +3,26 @@ module.exports = {
     Array: new Array()
 }
 
+Array.prototype.toTwenty = function(){
+    var arr20 = [];
+    for(var i = 1; i<= 20; i++){
+        arr20.push(i);
+    }
+    return arr20;
+}
+
+Array.prototype.toOneThousand = function(){
+    var arr1000 = [];
+    for(var i = 10; i<= 1000; i+=10){
+        arr1000.push(i);
+    }
+    return arr1000;
+}
+
+Array.prototype.toForty = function(){
+    var arr40 = [];
+    for(var i = 2; i<= 40; i+=2){
+        arr40.push(i);
+    }
+    return arr40;
+}
